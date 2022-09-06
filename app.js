e56478ba8b4d312978b75ff29e28362903372701
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-09-07 00:57:20 +0500

    add calculation functions

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const test = () => {
-    alert('Testdsd')
-} 
+const add = () => {
+    let userInputOne = document.querySelector('#add_input_1').value;
+    let userInputTwo = document.querySelector('#add_input_2').value;
+    let result = +userInputOne + +userInputTwo;
+    document.querySelector('#result-add').innerHTML = result;
+}
+
+const subtract = () => {
+    let userInputOne = document.querySelector('#subtract_input_1').value;
+    let userInputTwo = document.querySelector('#subtract_input_2').value;
+    let result = +userInputOne - +userInputTwo;
+    document.querySelector('#result-sub').innerHTML = result;
+}
+
+const multiply = () => {
+    let userInputOne = document.querySelector('#multiply-input_1').value;
+    let userInputTwo = document.querySelector('#multiply-input_2').value;
+    let result = +userInputOne * +userInputTwo;
+    document.querySelector('#result-mul').innerHTML = result;
+}
+
+const divide = () => {
+    let userInputOne = document.querySelector('#divide_input_1').value;
+    let userInputTwo = document.querySelector('#divide_input_2').value;
+    let result = +userInputOne / +userInputTwo;
+    document.querySelector('#result-divide').innerHTML = result;
+}
